002039a91bf6a33301e669c3fc55f6a1923df426
Isavenko <51270435+Isavenko@users.noreply.github.com>
2020-10-02 18:51:10 +0200

    Update script.php

--- script.php
+++ script.php
@@ -1,7 +1,5 @@
 <?php
-header('Content-Type: text/xml');
-echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
-echo '<response>';
-	echo '<b>hi</b>';
-echo '</response>';
+
+echo 'response';
+
 ?>
